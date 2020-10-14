28e6a99b5f12b981ef379b0fe772987dd75dba7f
jbgreenh <48000025+jbgreenh@users.noreply.github.com>
2020-10-13 19:57:39 -0700

    Added basic movement for player character

---
+++ NASA Web-Based Game 15A/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 25;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * 25;
+        transform.Translate(x,y,0);
+    }
+}
