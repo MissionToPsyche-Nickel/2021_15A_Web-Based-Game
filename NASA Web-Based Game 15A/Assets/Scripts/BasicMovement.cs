d24b0b104572f35f48b9e35ae90966d41d9ad4e0
Sadman Hossain <SadmanHossain@hotmail.com>
2020-10-14 02:48:41 -0700

    Altered movement, clamping, etc
    - Changed movement to work with with RigidBody2D  to account for collision interactions
    - Added screen claming so Psyche stays within screen bounds
    - Attached Basic Movement.css and Body Clamping.css to RigidBody2D component on Psyche
    - Added temporary Background Object

--- NASA Web-Based Game 15A/Assets/Scripts/BasicMovement.cs
+++ NASA Web-Based Game 15A/Assets/Scripts/BasicMovement.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public Vector2 movement;
+  
     // Start is called before the first frame update
     void Start()
     {
-        
+	rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 25;
-        float y = Input.GetAxis("Vertical") * Time.deltaTime * 25;
-        transform.Translate(x,y,0);
+       movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+     
+    // FixedUpdate is called every physics detection step  
+    void FixedUpdate()
+    {
+	movePsyche(movement);
+    }
+
+    // Type of movement Psyche will have
+    // We can change this function if/when needed  
+    void movePsyche(Vector2 direction)
+    {
+	rb.MovePosition((Vector2)transform.position + (25 * direction * Time.deltaTime));
     }
 }
---
+++ NASA Web-Based Game 15A/Assets/Scripts/BoundaryClamping.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryClamping : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {   
+        // Clamps player object to size x by y screen 
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -10f, 10f),
+		    Mathf.Clamp(transform.position.y, -4f, 4f), transform.position.z);
+    }
+}
