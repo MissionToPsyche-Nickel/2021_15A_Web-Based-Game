9fb2162727215d0d42c405b497a38fda241d1662
jbgreenh <48000025+jbgreenh@users.noreply.github.com>
2021-02-08 18:19:29 -0700

    Update SpawnAsteroids.cs

--- NASA Web-Based Game 15A/Assets/Scripts/SpawnAsteroids.cs
+++ NASA Web-Based Game 15A/Assets/Scripts/SpawnAsteroids.cs
@@ -9,6 +9,9 @@ public class SpawnAsteroids : MonoBehaviour
     public float asteroidSpawnTimeMin = 1f;
     public float asteroidSpawnTimeMax = 3f;
     public float lastAsteroidTime = -100f;
+    private float astSize = 0; 
+    //private float w = Screen.width;
+    //private float h = Screen.height;
  
     // Update is called once per frame
     void Update()
@@ -18,7 +21,15 @@ public class SpawnAsteroids : MonoBehaviour
         {
             lastAsteroidTime = Time.time;
             GameObject asteroid = Instantiate(asteroidPrefab);
-            asteroid.transform.position = new Vector3(Random.Range(gameObject.GetComponent<GameStart>().upperLeftXY.x, gameObject.GetComponent<GameStart>().upperRightXY.x), gameObject.GetComponent<GameStart>().upperRightXY.y, 0);
+            astSize = Random.Range(.01f, .05f);
+            asteroid.transform.localScale = new Vector2(astSize, astSize);
+            asteroid.transform.position = new Vector3(Random.Range(-10, 10), transform.position.y + 6, 0);
+            
+
+            /*GameObject asteroid = Instantiate(asteroidPrefab);
+            asteroid.transform.position = new Vector3(Random.Range(gameObject.GetComponent<GameStart>().upperLeftXY.x, 
+            	gameObject.GetComponent<GameStart>().upperRightXY.x), gameObject.GetComponent<GameStart>().upperRightXY.y, 0);*/
+
             asteroid.GetComponent<Rigidbody2D>().velocity = new Vector3(0, -1, 0) * 0.1f;
         }        
     }
