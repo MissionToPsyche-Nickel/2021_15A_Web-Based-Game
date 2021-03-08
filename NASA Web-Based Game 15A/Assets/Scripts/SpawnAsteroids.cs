b4d38d9ceefcff584142310c52b245e5b1bd5662
jbgreenh <48000025+jbgreenh@users.noreply.github.com>
2021-03-08 04:10:12 -0700

    difficulty now increases over time

--- NASA Web-Based Game 15A/Assets/Scripts/SpawnAsteroids.cs
+++ NASA Web-Based Game 15A/Assets/Scripts/SpawnAsteroids.cs
@@ -6,9 +6,10 @@ public class SpawnAsteroids : MonoBehaviour
 {
 
     public GameObject asteroidPrefab;
-    public float asteroidSpawnTimeMin = 1f;
-    public float asteroidSpawnTimeMax = 3f;
+    private float asteroidSpawnTimeMin = 4f;
+    private float asteroidSpawnTimeMax = 8f;
     public float lastAsteroidTime = -100f;
+    private float newTime = -100f;
     private float astSize = 0; 
     //private float w = Screen.width;
     //private float h = Screen.height;
@@ -32,7 +33,29 @@ public class SpawnAsteroids : MonoBehaviour
             asteroid.transform.localScale = new Vector2(astSize, astSize);
             asteroid.transform.position = new Vector3(Random.Range(leftSide, rightSide), transform.position.y + 6, 0);
             asteroid.GetComponent<Rigidbody2D>().velocity = new Vector3(Random.Range(-4f, 4f), Random.Range(-10f, -.3f), 0).normalized * Random.Range(.1f, 10f);
-            asteroids.Add(asteroid);
-        }        
+            asteroids.Add(asteroid);	
+        }
+
+
+        //difficulty increases over time
+        if (Time.time - newTime > 1f)
+        {
+	        newTime = Time.time;
+	        asteroidSpawnTimeMin = asteroidSpawnTimeMin - .05f;
+	        asteroidSpawnTimeMax = asteroidSpawnTimeMax - .05f; 
+	        print("min: " + asteroidSpawnTimeMin);
+	        print("max: " + asteroidSpawnTimeMax);
+	        if (asteroidSpawnTimeMin < .3f)
+	        {
+	        	asteroidSpawnTimeMin = .3f;
+	        }
+
+	        if (asteroidSpawnTimeMax < 1f)
+	        {
+	        	asteroidSpawnTimeMax = 1f;
+	        }
+   		}
+
+
     }
 }
