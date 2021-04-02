a6f0a2d63029ad1fa909b5e253ba37711b8197fe
Ryan Hill <rmhill10@asu.edu>
2021-04-02 11:47:04 -0700

    Basic Unit Testing

---
+++ NASA Web-Based Game 15A/Assets/Tests/EditMode/EditModeTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class EditModeTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void EditModeTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
---
+++ NASA Web-Based Game 15A/Assets/Tests/PlayMode/SceneTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class SceneTest
+    {
+        [UnityTest]
+        public IEnumerator PlayScene()
+        {
+            var gameObject = new GameObject();
+            var sceneLoader = gameObject.AddComponent<MainMenu>();
+
+            sceneLoader.PlayGame();
+
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(SceneManager.GetActiveScene().name, "GameLevel");
+        }
+        [UnityTest]
+        public IEnumerator ScoreboardScene()
+        {
+            var gameObject = new GameObject();
+            var sceneLoader = gameObject.AddComponent<MainMenu>();
+
+            sceneLoader.ScoreBoard();
+
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(SceneManager.GetActiveScene().name, "Scoreboard");
+        }
+
+        [UnityTest]
+        public IEnumerator OptionsScene()
+        {
+            var gameObject = new GameObject();
+            var sceneLoader = gameObject.AddComponent<MainMenu>();
+
+            sceneLoader.Options();
+
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(SceneManager.GetActiveScene().name, "Options");
+        }
+
+        [UnityTest]
+        public IEnumerator HowToPlayScene()
+        {
+            var gameObject = new GameObject();
+            var sceneLoader = gameObject.AddComponent<MainMenu>();
+
+            sceneLoader.Tutorial();
+
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(SceneManager.GetActiveScene().name, "HowToPlay");
+        }
+    }
+}
